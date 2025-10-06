6b9a5222ea9ac9a5efbd50f6f2894b6222abf6f7
Peter Davison <davison.pl@gmail.com>
2025-10-05 22:40:05 -0400

    Add section to blr-shared-frontend to pass through bootsrap classes

--- blr-shared-frontend/src/index.js
+++ blr-shared-frontend/src/index.js
@@ -7,3 +7,7 @@ export { getValidAccessToken,
   spinnerOn,
   spinnerOff,
  } from './shared.js';
+
+ // Add any specific bootstrap components that get consumed downstream. Need to do this because
+ // re-import of bootstrap downstream causes navbar toggles to stop working.
+ export { Modal, Collapse } from 'bootstrap';   
